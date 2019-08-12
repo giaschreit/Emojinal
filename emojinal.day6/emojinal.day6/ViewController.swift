df91ae2e48cd29ac10827e98c4c3982be9845bf1
KWK <giaschreitmueller@Apples-MacBook-Pro-94.local>
2019-08-12 11:19:09 -0400

    Completed Iteration 2

--- emojinal.day6/emojinal.day6/ViewController.swift
+++ emojinal.day6/emojinal.day6/ViewController.swift
@@ -9,13 +9,29 @@
 import UIKit
 
 class ViewController: UIViewController {
+let emojis = ["😴": "sleepy monday", "🤗": "energetic"]
 
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+@IBAction func showMessage(sender: UIButton) {
+// let alertController = UIAlertController(title: "\(titleText)", message: "\(messageText)", preferredStyle: UIAlertController.Style.alert)
+    
+    if let selectedEmotion = sender.titleLabel?.text {
+        let titleText = "Sleepy Monday"
+        let messageText = "Come on! You can get through the day!"
+        
+        let alertController = UIAlertController(title:"\(titleText)", message: "\(messageText)", preferredStyle:UIAlertController.Style.alert)
+        
+     alertController.addAction(UIAlertAction(title: "OK", style:UIAlertAction.Style.default, handler: nil))
+        present(alertController, animated: true, completion:nil)
+    
+}
 }
 
-
+}
